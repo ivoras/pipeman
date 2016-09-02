230d8f8ea75079d0aca47adcce7b570acd83db6c
Davor Kapsa <davorkapsa@gmail.com>
2016-09-02 21:49:28 +0200

    fix cyclomatic complexity in checkConfig

--- config.go
+++ config.go
@@ -44,24 +44,7 @@ func checkConfig() {
 	if Cfg.Port < 0 {
 		log.Fatalln("port must be a positive integer")
 	}
-	for _, pd := range Cfg.Network {
-		if pd.Loss < 0 {
-			log.Fatalln("Lost must be a positive decimal number")
-		}
-		if len(pd.Jitter) != 0 {
-			if len(pd.Jitter) != 2 {
-				log.Fatalln("Jitter must be specified as an array of 2 numbers: (#num1 +/- #num2) milliseconds")
-			}
-			for _, j := range pd.Jitter {
-				if j < 0 {
-					log.Fatalln("Jitter spec must be positive integers")
-				}
-			}
-			if pd.Jitter[1] > pd.Jitter[0] {
-				log.Fatalln("Jitter must be specified as an array of 2 numbers: (#num1 +/- #num2) milliseconds, #num2 < #num1")
-			}
-		}
-	}
+	checkConfigDomain()
 }
 
 func checkConfigDomain() {
